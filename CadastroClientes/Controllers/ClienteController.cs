6357719964ccd78bca1772851066d6c3b0baeb8a
Marcela Mendonça <marcelamendonca132@gmail.com>
2023-06-21 16:45:55 -0300

    Projeto-MVC

---
+++ CadastroClientes/Controllers/ClienteController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroClientes.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+    public class ClienteController : Controller
+    {
+        public IActionResult Criar()
+        {
+            return View();
+        }
+    }
+    public class ClienteController : Controller
+    {
+        public IActionResult Atualizar()
+        {
+            return View();
+        }
+    }
+    public class ClienteController : Controller
+    {
+        public IActionResult Apagar()
+        {
+            return View();
+        }
+    }
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
